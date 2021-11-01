9454b92b7e8570db44a5ac5e95ea447bbac6f1be
umamahale <umagasmos@gmail.com>
2021-11-01 08:57:16 -0400

    first message

---
+++ ParalellTaskDemo/ParalellTaskDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParalellTaskDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
